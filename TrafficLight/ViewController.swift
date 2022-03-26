473522fd1161ee4e6dc99a42bd9ab78d0947a45d
malinatrash <malinatrashh@gmail.com>
2022-03-26 18:14:22 +0800

    main task completed

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -9,11 +9,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var redLightView: UIView!
+    @IBOutlet weak var yellowLightView: UIView!
+    @IBOutlet weak var greenLightView: UIView!
+    
+    @IBOutlet weak var startButton: UIButton!
+    
+    private var allLightsDisable = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        redLightView.alpha = 0.3
+        yellowLightView.alpha = 0.3
+        greenLightView.alpha = 0.3
+        
+        startButton.layer.cornerRadius = 25
+        
+        redLightView.layer.cornerRadius = 90
+        yellowLightView.layer.cornerRadius = 90
+        greenLightView.layer.cornerRadius = 90
     }
 
 
+    @IBAction func changeLight() {
+        
+        if allLightsDisable || greenLightView.alpha == 1{
+            greenLightView.alpha = 0.3
+            redLightView.alpha = 1
+            startButton.setTitle("NEXT", for: .normal)
+            allLightsDisable = false
+            return
+        }
+        
+        if redLightView.alpha == 1 {
+            redLightView.alpha = 0.3
+            yellowLightView.alpha = 1
+            return
+        }
+        
+        if yellowLightView.alpha == 1 {
+            yellowLightView.alpha = 0.3
+            greenLightView.alpha = 1
+            return
+        }
+    }
 }
 
